5f0f6887f3f58c520a0d977b4fdaf106dbb1c795
towhiid <towhid3008@gmail.com>
2020-08-12 12:32:01 +0600

    template got

--- template.js
+++ template.js
@@ -1,4 +1,17 @@
 const firstName = "Abu";
 const lastNAme = "Salah";
-const fullName = firstName +" " + lastNAme + " " + "is a good boy." ;
-console.log(fullName);
+// const fullName = firstName +" " + lastNAme + " " + "is a good boy." ;
+// after es6
+const fullName = `${firstName} ${lastNAme} ${30+498+32} is a good boy.`;
+
+const multiLine = "you are good boy\n"
+ + "YOu are the motivator\n" 
+ + "You are a simple man";
+
+// console.log(fullName);
+// console.log(multiLine);
+// after es6
+const multiLine2 = `You are a good boy.
+i know you very well,
+and you are well behaved.`
+console.group(multiLine2);
