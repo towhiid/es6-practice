0df4e42d47587590adf8a5e29a1295f4a0f7544f
towhiid <towhid3008@gmail.com>
2020-08-12 13:15:24 +0600

    sread learning

---
+++ threedots.js
@@ -0,0 +1,17 @@
+// const ages = [23, 12, 22, 11];
+// const ages2 = [32, 21, 23];
+// const ages3 = [30, 23, 43];
+// const allAges = ages.concat(ages2).concat([12]).concat(ages3);
+// const allAges2 = [...ages, ...ages2, 14, ...ages3];
+// console.log(allAges2);
+// console.log(allAges);
+
+const business = 340;
+const doctor = 240;
+const teacher = 200;
+const maximum = Math.max(business, doctor, teacher);
+console.log(maximum);
+const allMoney = [340, 342, 234];
+const max = Math.max(...allMoney);
+console.log(max);
+
