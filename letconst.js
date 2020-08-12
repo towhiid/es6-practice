802c8d90eb5af470a2e8626c93e40bfae63fc5e3
towhiid <towhid3008@gmail.com>
2020-08-12 11:25:24 +0600

    let learning with const

---
+++ letconst.js
@@ -0,0 +1,10 @@
+const nums = [23, 34, 74];
+nums[2] = 55;
+nums.push(22);
+const student = {name: "arafat", age: "30"};
+// console.log(nums);
+// console.log(student);
+
+let patientName = "mazeda khala"; //let = leakproof
+patientName = "Himu";
+console.log(patientName);
