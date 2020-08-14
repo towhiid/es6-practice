916c391eeb37b62d0cb6884fe1059e9d47c69eee
towhiid <towhid3008@gmail.com>
2020-08-15 01:08:39 +0600

    class

--- class.js
+++ class.js
@@ -1,6 +1,11 @@
 class Student{
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "Balithuba High School";
 
+    }
 }
-const student1 = new Student();
-const student2 = new Student();
+const student1 = new Student(23, "amit");
+const student2 = new Student(32, "tanvir");
 console.log(student1, student2);
